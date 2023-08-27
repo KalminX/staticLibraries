f99d085e8ee76af7943552e0c055458484643381
KalminX <noriabure.com>
2023-08-27 03:02:35 -0800

    Just having fun learning about static libraries

---
+++ math_functions.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "math_functions.h"
+/**
+* add - add two numbers
+* @a: the first number
+* @b: the second number
+* Return: return the results for each function
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* subtract - subtract two numbers
+* @a: the first number
+* @b: the second number
+* Return: returns the difference
+*/
+int subtract(int a, int b)
+{
+	return (a - b);
+}
+/**
+* multiply - multiply two numbers
+* @a: the first number
+* @b: the second number
+* Return: returns the product
+*/
+int multiply(int a, int b)
+{
+	return (a * b);
+}
+/**
+* divide - divide the first number by the second number
+* @a: the first number
+* @b: the second number
+* Return: returns the quotient
+*/
+int divide(int a, int b)
+{
+	return (a / b);
+}
---
+++ math_functions.h
@@ -0,0 +1,11 @@
+/*math_functions.h*/
+
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+#endif
